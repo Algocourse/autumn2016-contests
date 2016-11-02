9626e68d75e295f7982aae51d7d4fdda7d9216b4
bdzl <vladb15@mail.ru>
2016-11-02 22:25:07 +0300

    added merge_sort based on simple merge impl

--- sorts/merge_sort.cpp
+++ sorts/merge_sort.cpp
@@ -3,6 +3,44 @@
 #include <algorithm>
 using namespace std;
 
+/*
+void sort(vector<int>& a, int l, int r) {
+    if (l == r) {
+        return;
+    }
+
+    int m = (l + r) >> 1;
+    sort(a, l, m);
+    sort(a, m + 1, r);
+
+    vector<int> buffer;
+    buffer.reserve(r - l + 1);
+
+    {
+        int p1, p2;
+        for (p1 = l, p2 = m + 1; p1 <= m && p2 <= r; ) {
+            if (a[p1] <= a[p2]) {
+                buffer.push_back(a[p1]);
+                ++p1;
+            } else {
+                buffer.push_back(a[p2]);
+                ++p2;
+            }
+        }
+        while (p1 <= m) {
+            buffer.push_back(a[p1]);
+            ++p1;
+        }
+        while (p2 <= r) {
+            buffer.push_back(a[p2]);
+            ++p2;
+        }
+    }
+
+    copy(buffer.begin(), buffer.end(), a.begin() + l);
+}
+*/
+
 void sort(vector<int>& a, int l, int r) {
     if (l == r) {
         return;
